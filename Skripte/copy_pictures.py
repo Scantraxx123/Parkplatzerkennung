fe6a13e4c2c01e81e03af192b4e21df057697290
Arbeitslaptop <F.Willrich@ostfalia.de>
2019-04-05 13:37:44 +0200

    Hilfsskript zum Bilder kopieren erstellt

---
+++ Skripte/copy_pictures.py
@@ -0,0 +1,21 @@
+import os
+
+
+source = ""
+dest_empty = ""
+dest_occ = ""
+
+for dirname, dirnames, filenames in os.walk(source):
+    for subdirname in dirnames:
+        if(subdirname == "Empty"):
+            for file_name in os.listdir(os.path.join(dirname, subdirname)):
+                shutil.copy(os.path.join(dirname, subdirname,file_name), dest_empty)
+        if(subdirname == "Occupied"):
+            for file_name in os.listdir(os.path.join(dirname, subdirname)):
+                shutil.copy(os.path.join(dirname, subdirname,file_name), dest_occ)            
+
+        
+            
+        
+            
+        
